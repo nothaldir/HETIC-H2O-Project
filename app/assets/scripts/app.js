cead97ea599e5881bfa15f4d907d66556f89d7c7
Camille Basset <camille.basset@hotmail.fr>
2016-11-20 23:24:27 +0100

    feat(parallax effect on map)

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -233,3 +233,22 @@ function getJSON(url, callback) {
     console.log(mapId);
 } // End of getJSON function
 
+
+
+$(window).on('mousemove', function(e) {
+    var w = $(window).width();
+    var h = $(window).height();
+    var offsetX = 0.5 - e.pageX / w;
+    var offsetY = 0.5 - e.pageY / h;
+
+    $(".parallax").each(function(i, el) {
+        var offset = parseInt($(el).data('offset'));
+        var translate = "translate3d(" + Math.round(offsetX * offset) + "px," + Math.round(offsetY * offset) + "px, 0px)";
+
+        $(el).css({
+            '-webkit-transform': translate,
+            'transform': translate,
+            'moz-transform': translate
+        });
+    });
+});
