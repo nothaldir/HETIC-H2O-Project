326bcf03f5e13949c1e85fc966ef129a5306dc3f
Gala Pillot <gala.pill@gmail.com>
2016-11-21 20:19:45 +0100

    add(feat): function initRegion

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -108,12 +108,6 @@ function map(){
     mapId = $(this).attr('id').split('-');
     console.log(mapId[0]+','+mapId[1]);
       initRegion();
-    getJSON("/datas/"+mapId[0]+".json", function(data) {
-      console.log(data.continent[0].id_continent);
-      for (var i=1; i <= (data.continent.length - 1); i++) {
-        console.log(data.continent[i].id_country);
-      }
-    });
   });
   $('.Map #world_map g').hover(
     function(){
@@ -137,8 +131,103 @@ function map(){
 
 // initialize region page with data
 function initRegion() {
-        $('.Region').addClass('Region--open');
-        $('.Map').hide();
+  $('.Region').addClass('Region--open');
+  $('.Map').hide();
+
+  getJSON("datas/"+mapId[0]+".json", function(data) {
+
+    // grab container
+    var container = document.querySelector('.Region-container');
+    // remove all
+    container.innerHTML = "";
+
+    // creation of section for continent
+    continent = document.createElement('section');
+    continent.setAttribute('id', 'section');
+    continent.classList.add('Region-container-section');
+    container.appendChild(continent);
+
+    // ID
+    var continentId = document.createElement('h2');
+    continentId.innerHTML = data.continent[0].id_continent;
+    continent.appendChild(continentId);
+
+    //INFO
+    var continentInfo = document.createElement('div');
+    continentInfo.classList.add('Region-container-info');
+    continent.appendChild(continentInfo);
+
+    //TITLE
+    var continentTitle = document.createElement('h3');
+    continentTitle.classList.add('Region-container-title');
+    continentTitle.innerHTML = data.continent[0].title;
+    continentInfo.appendChild(continentTitle);
+
+    //SUBTITLE
+    var continentSubtitle = document.createElement('h4');
+    continentSubtitle.classList.add('Region-container-subtitle');
+    continentSubtitle.innerHTML = data.continent[0].subtitle;
+    continentInfo.appendChild(continentSubtitle);
+
+    //TEXT CONTENT
+    var continentText = document.createElement('p');
+    continentText.classList.add('Region-container-text');
+    continentText.innerHTML = data.continent[0].content;
+    continentInfo.appendChild(continentText);
+
+    //BUTTON
+    var continentButton = document.createElement('button');
+    continentButton.classList.add('Region-container-watchButton');
+    continentButton.innerHTML = "Watch";
+    continentInfo.appendChild(continentButton);
+
+    // QUICK NAVIGATION
+      var region = document.querySelector('.Region');
+      var quickNav = document.createElement('nav');
+      quickNav.classList.add('Quick-navigation');
+      region.insertBefore(quickNav, container);
+      var scrollIndicator = document.createElement('div');
+      scrollIndicator.classList.add('Scroll-progress-indicator');
+      quickNav.appendChild(scrollIndicator);
+
+
+    for (var i=1; i <= (data.continent.length - 1); i++) {
+
+      // QUICK NAVIGATION ELEMENTS
+      var anchor = document.createElement('a');
+      anchor.setAttribute('href', '#section'+i);
+      anchor.classList.add('Quick-navigation-item');
+      quickNav.appendChild(anchor);
+
+      //SECTIONS
+      var country = document.createElement('section');
+      country.setAttribute('id', 'section'+i);
+      country.classList.add('Region-container-section');
+      container.appendChild(country);
+
+      //ID SECTIONS
+      var countryId = document.createElement('h2');
+      countryId.classList.add('Region-container-country');
+      countryId.innerHTML = data.continent[i].id_country;
+      country.appendChild(countryId);
+
+      //SUBTITLE SECTIONS
+      var countrySubtitle = document.createElement('h4');
+      countrySubtitle.classList.add('Region-container-subtitle');
+      countrySubtitle.innerHTML = data.continent[i].subtitle;
+      country.appendChild(countrySubtitle);
+
+      // TEXT CONTENT SECTIONS
+      var countryText = document.createElement('p');
+      countryText.classList.add('Region-container-text');
+      countryText.innerHTML = data.continent[i].content;
+      country.appendChild(countryText);
+
+
+      // document.getElementByClassName('Region-container').insertBefore(nav);
+      // var nav = document.createElement("nav").classList.add('Quick-navigation');
+    }
+  });
 };
 
 /*move();
@@ -201,9 +290,9 @@ function hideContinent() {
     if($(window).width() < 768)
     {
         // stick continent name on top
-        $("#section1 .Region-container-continent").addClass('Region-container-continent--fixed');
+        $("#section .Region-container-continent").addClass('Region-container-continent--fixed');
         // adapt page
-        $("#section1 .Region-container-info").css("padding-top","90px");
+        $("#section .Region-container-info").css("padding-top","90px");
         $("section:last-child").css("margin-bottom","100px");
     } else {}
 };
@@ -235,17 +324,7 @@ function getJSON(url, callback) {
 
 
 /* 
-var continent = document.getElementsByClassName(Region-container-continent);
-continent.innerHTML = data.continent[0].id_continent;
 
-document.getElementsByClassName(Region-container-continent).innerHTML = data.continent[0].id_continent;
-
-var title = document.getElementsByClassName(Region-container-title);
-title.innerHTML = data.continent[0].title;
-var subtitle = document.getElementsByClassName(Region-container-title);
-subtitle.innerHTML = data.continent[0].subtitlt;
-var text = document.getElementsByClassName(Region-container-title);
-text.innerHTML = data.continent[0].content;
 
 boucle for
 
@@ -256,7 +335,28 @@ document.getElementById("container").appendChild(p);
 var continent = document.getElementsByClassName(Region-container-continent);
 continent.innerHTML = data.continent[0].id_continent;
 
-var nav = document.createElement("nav").classList.add('Quick-navigation');
 document.getElementByClassName('Region-container').insertBefore(nav);
+var nav = document.createElement("nav").classList.add('Quick-navigation');
 
+<!-- <section id="section2" class="Region-container-section">
+            <h2 class="Region-container-continent">Europe</h2>
+            <div class="Region-container-info">
+                <h2 class="Region-container-country">France</h2>
+                <h3 class="Region-container-title">A great potential</h3>
+                <h4 class="Region-container-subtitle">Europe is running out of fresh water and overexploit water supplies</h4>
+                <p class="Region-container-text">Europeans are extracting too much from rivers, lakes and underground water sources, which can take millennia to be replenished, according to an EEA report published at the World Water Forum in Istanbul. This has so far disguised the continent’s water shortage, but it is only a stop-gap as supplies will run out.</p>
+                <button class="Region-container-watchButton"><a></a>Watch</button>
+            </div>
+        </section>
+
+        <section id="section3" class="Region-container-section">
+            <h2 class="Region-container-continent">Europe</h2>
+            <div class="Region-container-info">
+                <h2 class="Region-container-country">UK</h2>
+                <h3 class="Region-container-title">A drinking water mess</h3>
+                <h4 class="Region-container-subtitle">Europe is running out of fresh water and overexploit water supplies</h4>
+                <p class="Region-container-text">Europeans are extracting too much from rivers, lakes and underground water sources, which can take millennia to be replenished, according to an EEA report published at the World Water Forum in Istanbul. This has so far disguised the continent’s water shortage, but it is only a stop-gap as supplies will run out.</p>
+                <button class="Region-container-watchButton"><a></a>Watch</button>
+            </div>
+        </section> -->
 */
